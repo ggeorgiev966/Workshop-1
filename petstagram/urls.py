b9405b0ccd1d764d1b47943fdacf473f26e193e9
Georgi Georgiev <123512842+ggeorgiev966@users.noreply.github.com>
2024-09-29 18:40:07 +0300

    feat: added pet model

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -4,7 +4,7 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('common/', include('petstagram.common.urls')),
+    path('', include('petstagram.common.urls')),
     path('accounts/', include('petstagram.accounts.urls')),
     path('pets/', include('petstagram.pets.urls')),
     path('photos/', include('petstagram.photos.urls'))
